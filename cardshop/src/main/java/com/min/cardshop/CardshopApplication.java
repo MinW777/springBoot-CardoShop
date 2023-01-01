52f59c5202ad90f1774fd18e65d2f57885520d83
Min_Lo <j3531269@gmail.com>
2023-01-01 14:18:07 +0800

    初始化springboot project

---
+++ cardshop/src/main/java/com/min/cardshop/CardshopApplication.java
@@ -0,0 +1,13 @@
+package com.min.cardshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CardshopApplication.class, args);
+    }
+
+}
---
+++ cardshop/src/test/java/com/min/cardshop/CardshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.min.cardshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
